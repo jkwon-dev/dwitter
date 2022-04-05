2123821325d17d4b0cde138ee00a69e4f5ead5c0
jkwon <kwonejk@gmail.com>
2022-04-05 22:05:13 +0800

    middleware - auth

--- router/auth.js
+++ router/auth.js
@@ -3,6 +3,7 @@ import {} from 'express-async-errors';
 import { body } from 'express-validator';
 import { validate } from '../middleware/validator.js';
 import * as authController from '../controller/auth.js'; 
+import { isAuth } from '../middleware/auth.js';
 
 const router = express.Router();
 
@@ -35,6 +36,6 @@ router.post('/signup', validateSignup, authController.singup);
 //post /login
 router.post('/login', validateCredential ,authController.login); 
 
-
+router.get('/me', isAuth, authController.me); 
 
 export default router; 

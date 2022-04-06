c1deb4f76fbdf0ef9e385d326e72e954096f720f
jkwon <kwonejk@gmail.com>
2022-04-06 17:34:04 +0800

    mysql - auth part

--- app.js
+++ app.js
@@ -26,5 +26,5 @@ app.use((error, req, res, next) => {
     res.sendStatus(500); 
 });
 
-db.getConnection().then((connection) => console.log(connection)); 
+//db.getConnection().then((connection) => console.log(connection)); 
 app.listen(config.host.port); 
